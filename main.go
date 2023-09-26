e5c3c58e924d0b4737f61acb22be1aa1542ec282
LivanaKi <natzasha2015@gmail.com>
2023-09-26 20:21:00 +0300

    Changes to be committed: 	modified:   clients/telegram/telegram.go 	new file:   consumer/consumer.go 	new file:   consumer/event-consumer/event-consumer.go 	renamed:    events/telegram/commands.go -> events/telegram2/commands.go 	renamed:    events/telegram/messages.go -> events/telegram2/messages.go 	renamed:    events/telegram/telegram.go -> events/telegram2/telegram.go 	modified:   main.go

--- main.go
+++ main.go
@@ -3,22 +3,32 @@ package main
 import (
 	"flag"
 	"log"
+
 	"github.com/Users/natza/telegaBot/clients/telegram"
+	eventconsumer "github.com/Users/natza/telegaBot/consumer/event-consumer"
+	"github.com/Users/natza/telegaBot/events/telegram2"
+	"github.com/Users/natza/telegaBot/storage/files"
 )
 
 const(
 	tgBotHost = "api.telegram.org"
+	storagePath = "storage"
+	batchSize = 100
 )
 
 func main() {
 
-	tgClient = telegram.New(tgBotHost, mustToken())
+	evetsProcessor := telegram2.New(
+		telegram.New(tgBotHost, mustToken()),
+		files.New(storagePath),
+	)
 
-	// fetcher = fetcher.New(tgClient)
+	log.Print("service started")
 
-	//processor = processor.New(tgClient)
-
-	// consumer.Start(fetcher, processor)
+	consumer := eventconsumer.New(evetsProcessor, evetsProcessor, batchSize)
+	if err := consumer.Start(); err != nil{
+		log.Fatal("service is stopped", err)
+	}
 }
 
 func mustToken() string {
