2e4baa7da4d37887e3d3f1a29e77bcbee9e4993e
LivanaKi <natzasha2015@gmail.com>
2023-09-27 15:53:37 +0300

    Changes to be committed: 	modified:   main.go

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/Users/natza/telegaBot/storage/files"
 )
 
+//6336153425:AAEmtiGS6iZead1F_EK7ZPo3c3taeccSZuc
+
 const(
 	tgBotHost = "api.telegram.org"
 	storagePath = "storage"
